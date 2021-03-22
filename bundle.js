be23c5070946493ff233c1005cd14ca53856956c
lessuiz <leonardoparreira2018@gmail.com>
2021-03-22 16:39:53 -0300

    initial commit

---
+++ bundle.js
@@ -0,0 +1,32 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ (() => {
+
+eval("throw new Error(\"Module build failed (from ./node_modules/babel-loader/lib/index.js):\\nSyntaxError: /home/leonardo/react-projects/react-list/src/index.js: Support for the experimental syntax 'jsx' isn't currently enabled (7:7):\\n\\n\\u001b[0m \\u001b[90m  5 |\\u001b[39m   render() {\\u001b[0m\\n\\u001b[0m \\u001b[90m  6 |\\u001b[39m     \\u001b[36mreturn\\u001b[39m (\\u001b[0m\\n\\u001b[0m\\u001b[31m\\u001b[1m>\\u001b[22m\\u001b[39m\\u001b[90m  7 |\\u001b[39m       \\u001b[33m<\\u001b[39m\\u001b[33mh1\\u001b[39m\\u001b[33m>\\u001b[39molá mundo\\u001b[33m<\\u001b[39m\\u001b[33m/\\u001b[39m\\u001b[33mh1\\u001b[39m\\u001b[33m>\\u001b[39m\\u001b[0m\\n\\u001b[0m \\u001b[90m    |\\u001b[39m       \\u001b[31m\\u001b[1m^\\u001b[22m\\u001b[39m\\u001b[0m\\n\\u001b[0m \\u001b[90m  8 |\\u001b[39m     )\\u001b[0m\\n\\u001b[0m \\u001b[90m  9 |\\u001b[39m   }\\u001b[0m\\n\\u001b[0m \\u001b[90m 10 |\\u001b[39m }\\u001b[33m;\\u001b[39m\\u001b[0m\\n\\nAdd @babel/preset-react (https://git.io/JfeDR) to the 'presets' section of your Babel config to enable transformation.\\nIf you want to leave it as-is, add @babel/plugin-syntax-jsx (https://git.io/vb4yA) to the 'plugins' section to enable parsing.\\n    at Parser._raise (/home/leonardo/react-projects/react-list/node_modules/@babel/parser/lib/index.js:775:17)\\n    at Parser.raiseWithData (/home/leonardo/react-projects/react-list/node_modules/@babel/parser/lib/index.js:768:17)\\n    at Parser.expectOnePlugin (/home/leonardo/react-projects/react-list/node_modules/@babel/parser/lib/index.js:9714:18)\\n    at Parser.parseExprAtom (/home/leonardo/react-projects/react-list/node_modules/@babel/parser/lib/index.js:11085:22)\\n    at Parser.parseExprSubscripts (/home/leonardo/react-projects/react-list/node_modules/@babel/parser/lib/index.js:10668:23)\\n    at Parser.parseUpdate (/home/leonardo/react-projects/react-list/node_modules/@babel/parser/lib/index.js:10648:21)\\n    at Parser.parseMaybeUnary (/home/leonardo/react-projects/react-list/node_modules/@babel/parser/lib/index.js:10626:23)\\n    at Parser.parseExprOps (/home/leonardo/react-projects/react-list/node_modules/@babel/parser/lib/index.js:10487:23)\\n    at Parser.parseMaybeConditional (/home/leonardo/react-projects/react-list/node_modules/@babel/parser/lib/index.js:10461:23)\\n    at Parser.parseMaybeAssign (/home/leonardo/react-projects/react-list/node_modules/@babel/parser/lib/index.js:10424:21)\");\n\n//# sourceURL=webpack://react-list/./src/index.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module doesn't tell about it's top-level declarations so it can't be inlined
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/index.js"]();
+/******/ 	
+/******/ })()
+;
